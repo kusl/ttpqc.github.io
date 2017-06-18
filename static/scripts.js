7ae9d8b7cdb85acee12f0abd78a4a9a18489246a
Kushal Hada (developer) <kushaldeveloper@gmail.com>
2017-06-18 11:21:07 -0400

    add all changes
    Signed-off-by: Kushal Hada (developer) <kushaldeveloper@gmail.com>

--- static/scripts.js
+++ static/scripts.js
@@ -1,52 +1,60 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-    console.log('Lets get started!');
-    $("#queryString").keyup(function() {
-        console.log($("#queryString")[0].value);
-        trackSearch($("#queryString")[0].value);
-    });
-    $("#queryString").keypress(function(e) {
+    let inputTextbox = document.getElementById("queryString")
+    inputTextbox.addEventListener('keydown', (function(e) {
         if (e.which == 13) {
             console.log("Please don't press enter. Be kind to the ajax.");
             event.preventDefault();
             return false;
         }
-    });
-});
+    }));
 
-function trackSearch(queryString) {
-    var searchUrl = `https://freemusicarchive.org/api/trackSearch?q=${queryString}&api_key=BESSHG06KZV7PRPT`;
-    // var searchUrl = "https://freemusicarchive.org/api/trackSearch?q=deerhoof&limit=10";
-    $.getJSON(searchUrl, function(dataset) {
-        var newHtmlString = "<ul>";
-        dataset.aRows.forEach(function(entry) {
-            let trackNumber = getTrackNumber(entry);
-            console.log(`The track number is ${trackNumber}`);
-            if (null != trackNumber) {
-                var trackData = getTrackData(trackNumber);
-                console.log(`The track url is ${trackData[0].track_file}`);
+    inputTextbox.addEventListener('keypress', (function(e) {
+        let searchUrl = `https://freemusicarchive.org/api/trackSearch?q=${inputTextbox.value}&api_key=BESSHG06KZV7PRPT`;
+        console.log(searchUrl);
+        let request = new XMLHttpRequest();
+        request.open('GET', searchUrl, true);
+        request.onload = function() {
+            if (this.status >= 200 && this.status < 400) {
+                // Success!
+                let data = JSON.parse(this.response);
+                console.log(data);
+                let newHtmlString = "<ul>";
+                data.aRows.forEach(function(entry) {
+                    let trackNumber = entry.match(/\(([^)]*)\)[^(]*$/)[1];
+                    console.log(`The track number is ${trackNumber}`);
+                    if (null != trackNumber) {
+                        let searchUrl = `https://freemusicarchive.org/api/get/tracks.json?track_id=${trackNumber}&api_key=BESSHG06KZV7PRPT`;
+                        console.log(`${searchUrl}`);
+                        let request = new XMLHttpRequest();
+                        request.open('GET', searchUrl, true);
+                        request.onload = function() {
+                            if (this.status >= 200 && this.status < 400) {
+                                // Success!
+                                let data = JSON.parse(this.response);
+                                data.dataset.forEach(function(track) {
+                                    console.log(`The track title is ${track.track_title}`);
+                                    newHtmlString += `<li>${track.track_title}</li>`;
+                                    console.log(newHtmlString);
+                                })
+                            } else {
+                                // We reached our target server, but it returned an error
+                            }
+                        };
+                        request.onerror = function() {
+                            // There was a connection error of some sort
+                        };
+                        request.send();
+                    }
+                })
+                newHtmlString += "</ul>";
+                $('#results').html(newHtmlString);
+            } else {
+                // We reached our target server, but it returned an error
             }
-        })
-        newHtmlString += "</ul>";
-        $('#results').html(newHtmlString);
-    });
-}
-
-function getTrackNumber(queryString) {
-    var regExp = /.*\(\K[^)]+/;
-    var matches = queryString.match(/\(([^)]*)\)[^(]*$/)[1];
-    console.log(
-        `
-        The query string was ${queryString}. 
-        The result fo the regular expression matches, the track number is ${matches}. 
-        `
-    );
-    return matches;
-}
-
-function getTrackData(queryString) {
-    var searchUrl = `https://freemusicarchive.org/api/get/tracks.json?track_id=${queryString}&api_key=BESSHG06KZV7PRPT`;
-    console.log(`${searchUrl}`);
-    $.getJSON(searchUrl, function(data) {
-        return data.dataset;
-    });
-}
+        };
+        request.onerror = function() {
+            // There was a connection error of some sort
+        };
+        request.send();
+    }));
+});
